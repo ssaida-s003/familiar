c6769f65dd243be4d070c381425748db8f31beab
천세진 <sejinnnnnn@naver.com>
2024-03-12 11:12:48 +0900

    🌱 chore : styled components 적용

---
+++ frontend/src/styles/styled.d.ts
@@ -0,0 +1,11 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    accent1: string;
+    accent2: string;
+    accent3: string;
+    text: string;
+    bg1: string;
+  }
+}
---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  accent1: '#f5fbfc',
+  accent2: '#f8c291',
+  accent3: '#d35400',
+  text: '#000000',
+  bg1: '#ffffff',
+};
+
+export const darkTheme: DefaultTheme = {
+  accent1: '#121212',
+  accent2: '#bdc3c7',
+  accent3: '#2c3e50',
+  text: '#ffffff',
+  bg1: '#121212',
+};
