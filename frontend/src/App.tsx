2b7d1cd7d7229f6664d3fbf72dfcbb071bac9234
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-12 17:25:39 +0900

    🌱 chore : 불필요한 코드 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
-import './App.css';
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>Hi</div>;
+  return <div>Hi</div>
 }
 
-export default App;
+export default App
