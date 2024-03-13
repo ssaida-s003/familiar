9f107827dfa18fcf98264eaed38b6e241f1cdeba
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-13 10:33:48 +0900

    ✨ feat : 기본적인 router 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from '@pages/HomePage'
+
+const AuthRoutes = () => (
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+  </Routes>
+)
 
 function App() {
-  return <div>Hi</div>
+  const user = true
+  return <BrowserRouter>{user && <AuthRoutes />}</BrowserRouter>
 }
 
 export default App
