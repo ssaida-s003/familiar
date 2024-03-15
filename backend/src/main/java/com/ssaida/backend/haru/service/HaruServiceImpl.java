48b3140d5c871a536e66d6e24ade263a65d45df3
김예현 <qkdo12@naver.com>
2024-03-15 14:21:11 +0900

    ✨feat: 하루 일상 기록 POST 구현
    - SQL 쿼리 로그 설정

---
+++ backend/src/main/java/com/ssaida/backend/haru/service/HaruServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ssaida.backend.haru.service;
+
+import com.ssaida.backend.haru.dto.CreateRecordRequest;
+import com.ssaida.backend.haru.entity.DailyRecord;
+import com.ssaida.backend.haru.repository.RecordRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class HaruServiceImpl implements HaruService{
+
+    final RecordRepository recordRepository;
+    @Override
+    @Transactional
+    public void createRecord(CreateRecordRequest createRecordRequest) throws Exception {
+        DailyRecord dailyRecord =new DailyRecord(createRecordRequest.getMemeberId(),createRecordRequest.getContent());
+        recordRepository.save(dailyRecord);
+        //TODO: FASTAPI 연결 필요
+        String url = "test_url";
+        dailyRecord.updateUrl(url);
+    }
+}
