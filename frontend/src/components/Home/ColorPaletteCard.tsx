f4459d35ff7386833b6fbf08b2a909317b6cd29f
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-18 17:18:27 +0900

    ✨ feat : 문 번호를 props로 전달 받아 store에 선택된 색상과 함께 저장

--- frontend/src/components/Home/ColorPaletteCard.tsx
+++ frontend/src/components/Home/ColorPaletteCard.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { ColorType } from '@/types/Components/ColorPaletteType'
+import * as c from '@components/Home/style/ColorPaletteCardStyle'
+import { useColorStore } from '@stores/refrigerator'
 
 interface ColorPaletteCardProps {
   colorInfo: ColorType
+  quadrantNum: number
 }
 
-const ColorPaletteCard: React.FC<ColorPaletteCardProps> = ({ colorInfo }) => {
-  return <div>{colorInfo.colorName}</div>
+const ColorPaletteCard: React.FC<ColorPaletteCardProps> = ({ colorInfo, quadrantNum }) => {
+  const selectedColorCode = useColorStore(state => state.selectedColorCode)
+  const setSelectedColorCode = useColorStore(state => state.updateSelectedColorCode)
+
+  const handleClick = () => {
+    setSelectedColorCode(quadrantNum, colorInfo.colorCode)
+    console.log(selectedColorCode)
+  }
+
+  return (
+    <c.Container onClick={handleClick} cardcolor={colorInfo.colorCode}>
+      <c.ColorDiv cardcolor={colorInfo.colorCode} />
+      <c.ColorText>{colorInfo.colorName}</c.ColorText>
+    </c.Container>
+  )
 }
 export default ColorPaletteCard
