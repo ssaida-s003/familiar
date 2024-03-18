4b686a886bd70637fc77e0d7bbc37d608a6361f0
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-18 17:13:15 +0900

    🎨 style : 문 색상 선택 후 div에 색깔을 주도록 스타일 적용

--- frontend/src/pages/style/HomePageStyle.tsx
+++ frontend/src/pages/style/HomePageStyle.tsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+interface QuadrantsProps {
+  selectedcolor: string
+}
+
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
@@ -9,22 +13,44 @@ export const Container = styled.div`
 `
 
 export const RefrigeratorContainer = styled.div`
-  width: 330px;
+  width: 329px;
   height: 674px;
   background-image: url('/icon/icon_refrigerator.png');
   background-size: contain;
   background-position: center;
   position: relative;
   margin-top: 20px;
+  border: 0.1px solid black;
 
   @media (max-width: 340px), (max-height: 680px) {
     display: none;
   }
 `
-export const Quadrants1 = styled.div`
+export const Quadrants1 = styled.div<QuadrantsProps>`
   width: 49.5%;
   height: 52.8%;
   position: absolute;
-  background-color:;
-  margin: 0.4%;
+  left: -0.5px;
+  background-color: ${props => props.selectedcolor};
+  margin: 0.5%;
+`
+
+export const Quadrants2 = styled.div<QuadrantsProps>`
+  width: 49.4%;
+  height: 42.25%;
+  position: absolute;
+  bottom: 11.5px;
+  left: -0.5px;
+  background-color: ${props => props.selectedcolor};
+  margin: 0.5%;
+`
+
+export const Quadrants3 = styled.div<QuadrantsProps>`
+  width: 49.4%;
+  height: 42.25%;
+  position: absolute;
+  bottom: 11.5px;
+  right: -2px;
+  background-color: ${props => props.selectedcolor};
+  margin: 0.5%;
 `
