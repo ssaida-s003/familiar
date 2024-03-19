092f38011e1d5c2696a041d08d7dc934bdd7bb9c
김예현 <qkdo12@naver.com>
2024-03-19 12:58:06 +0900

    🌱chore: ddl auto 설정 및 createDailyRecord 수정
    엔티티 변경사항에 맞춰 api 수정

---
+++ backend/src/main/java/com/ssaida/backend/family/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.ssaida.backend.family.repository;
+
+import com.ssaida.backend.family.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Integer> {
+}
--- backend/src/main/java/com/ssaida/backend/haru/service/HaruServiceImpl.java
+++ backend/src/main/java/com/ssaida/backend/haru/service/HaruServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ssaida.backend.haru.service;
 
 import com.ssaida.backend.family.entity.Member;
+import com.ssaida.backend.family.repository.MemberRepository;
 import com.ssaida.backend.haru.dto.CreateRecordRequest;
 import com.ssaida.backend.haru.entity.DailyRecord;
 import com.ssaida.backend.haru.repository.RecordRepository;
@@ -14,14 +15,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class HaruServiceImpl implements HaruService{
 
     private final RecordRepository recordRepository;
+    private final MemberRepository memberRepository;
     @Override
     @Transactional
     public void createRecord(CreateRecordRequest createRecordRequest) throws Exception {
+        Member member=memberRepository.findById(createRecordRequest.getMemberId()).orElseThrow();
 
-//        DailyRecord dailyRecord =new DailyRecord(createRecordRequest.getMemeberId(),createRecordRequest.getContent());
-//        recordRepository.save(dailyRecord);
-//        //TODO: FASTAPI 연결 필요
-//        String url = "test_url";
-//        dailyRecord.updateUrl(url);
+        DailyRecord dailyRecord = DailyRecord.builder()
+                .member(member)
+                .content(createRecordRequest.getContent()).build();
+        recordRepository.save(dailyRecord);
+        //TODO: FASTAPI 연결 필요
+        String url = "test_url";
+        dailyRecord.updateUrl(url);
     }
 }
