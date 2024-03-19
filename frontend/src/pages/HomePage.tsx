2f130a40a1c795247460e6ce0d768dc69e15fd2e
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-19 15:18:22 +0900

    ✨ feat : 디스플레이 역할을 진행할 div 추가

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import * as h from '@pages/style/HomePageStyle'
 import DoorColorPalette from '@components/Home/DoorColorPalette'
 import { useColorStore } from '@stores/refrigerator'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 const HomePage = () => {
   const setSelectedColorCode = useColorStore(state => state.selectedColorCode)
@@ -14,6 +14,11 @@ const HomePage = () => {
   return (
     <h.Container>
       <h.RefrigeratorContainer>
+        <h.PanelContainer>
+          <h.Panel onClick={handleClick(0)} />
+        </h.PanelContainer>
+        <h.Quadrants0 onClick={handleClick(0)} $selectedColor={setSelectedColorCode[0]} />
+        {isShowPalette[0] && <DoorColorPalette quadrantNum={0} />}
         <h.Quadrants1 onClick={handleClick(1)} $selectedColor={setSelectedColorCode[1]} />
         {isShowPalette[1] && <DoorColorPalette quadrantNum={1} />}
         <h.Quadrants2 onClick={handleClick(2)} $selectedColor={setSelectedColorCode[2]} />
