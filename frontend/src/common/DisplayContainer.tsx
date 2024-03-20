ac22332012837a9894820eb56969639e9bc82166
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-20 13:30:22 +0900

    ✨ feat : panel display container 생성

---
+++ frontend/src/common/DisplayContainer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 148px;
+  height: 265px;
+  margin: 0;
+`
+
+interface DisplayContainerProps {
+  children: React.ReactNode
+}
+
+const DisplayContainer: React.FC<DisplayContainerProps> = ({ children }) => {
+  return <Container>{children}</Container>
+}
+
+export default DisplayContainer
