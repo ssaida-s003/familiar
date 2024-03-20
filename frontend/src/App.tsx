088f8dec11a86475b6984aebe64d3d6dd9fe600e
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-20 13:34:40 +0900

    ✨ feat : route 분리
    - display panel 관련 = /display
    - 배경화면만 보는 것 = /display/wallpapers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from '@pages/HomePage'
+import WallPapersPage from '@pages/WallPapersPage'
 
 const AuthRoutes = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
+    <Route path="/display" element={<WallPapersPage />} />
+    <Route path="/display/wallpapers" element={<WallPapersPage />} />
   </Routes>
 )
 
