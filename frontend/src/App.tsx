9c5ed33ec04077d70d7cb926266ed21b1d8509af
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-20 11:24:03 +0900

    ♻️ refactor : 의미 없는 router 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from '@pages/HomePage'
-import HomePage1 from '@pages/HomePage1'
 
 const AuthRoutes = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
-    <Route path="/1" element={<HomePage1 />} />
   </Routes>
 )
 
