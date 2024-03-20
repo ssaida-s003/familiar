300f75d2ff97959c2275229bdd66f5dd51a3c3bc
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-20 09:21:08 +0900

    ✨ feat : 중복되는 4분면 문 컴포넌트 문 번호를 인자로 넘겨받아 중복 해결

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -22,13 +22,13 @@ const HomePage = () => {
         <h.PanelContainer>
           <h.Panel onClick={handleClick(0)} />
         </h.PanelContainer>
-        <h.Quadrants0 onClick={handleClick(0)} $selectedColor={setSelectedColorCode[0]} />
+        <h.Quadrants onClick={handleClick(0)} $selectedColor={setSelectedColorCode[0]} $quadrantsNum={0} />
         {isShowPalette[0] && <DoorColorPalette quadrantNum={0} />}
-        <h.Quadrants1 onClick={handleClick(1)} $selectedColor={setSelectedColorCode[1]} />
+        <h.Quadrants onClick={handleClick(1)} $selectedColor={setSelectedColorCode[1]} $quadrantsNum={1} />
         {isShowPalette[1] && <DoorColorPalette quadrantNum={1} />}
-        <h.Quadrants2 onClick={handleClick(2)} $selectedColor={setSelectedColorCode[2]} />
+        <h.Quadrants onClick={handleClick(2)} $selectedColor={setSelectedColorCode[2]} $quadrantsNum={2} />
         {isShowPalette[2] && <DoorColorPalette quadrantNum={2} />}
-        <h.Quadrants3 onClick={handleClick(3)} $selectedColor={setSelectedColorCode[3]} />
+        <h.Quadrants onClick={handleClick(3)} $selectedColor={setSelectedColorCode[3]} $quadrantsNum={3} />
         {isShowPalette[3] && <DoorColorPalette quadrantNum={3} />}
       </h.RefrigeratorContainer>
     </h.Container>
--- frontend/src/pages/style/HomePageStyle.tsx
+++ frontend/src/pages/style/HomePageStyle.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 interface QuadrantsProps {
   $selectedColor: string
+  $quadrantsNum: number
 }
 
 export const Container = styled.div`
@@ -29,26 +30,28 @@ export const RefrigeratorContainer = styled.div`
   position: relative;
   margin-top: 20px;
   border: 0.1px solid black;
+  cursor: pointer;
 
   @media (max-width: 340px), (max-height: 680px) {
     display: none;
   }
-  cursor: pointer;
 `
 
-export const Quadrants0 = styled.div<QuadrantsProps>`
+export const Quadrants = styled.div<QuadrantsProps>`
   width: 163.5px;
-  height: 357.5px;
+  height: ${props => (props.$quadrantsNum === 0 || props.$quadrantsNum === 1 ? '357.5px' : '286.5px')};
   position: absolute;
-  top: -1px;
-  right: -2px;
+  top: ${props => (props.$quadrantsNum === 0 || props.$quadrantsNum === 1 ? '-1px' : 'auto')};
+  bottom: ${props => (props.$quadrantsNum === 2 || props.$quadrantsNum === 3 ? '11.5px' : 'auto')};
+  right: ${props => (props.$quadrantsNum === 0 || props.$quadrantsNum === 3 ? '-2px' : 'auto')};
+  left: ${props => (props.$quadrantsNum === 1 || props.$quadrantsNum === 2 ? '-1.6px' : 'auto')};
   background-color: ${props => props.$selectedColor};
   margin: 0.5%;
 `
 
 export const PanelContainer = styled.div`
-  width: 163px;
-  height: 357px;
+  width: 163.5px;
+  height: 357.5px;
   position: absolute;
   top: -1px;
   right: -2px;
@@ -56,41 +59,12 @@ export const PanelContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin: 0.5%;
 `
 
 export const Panel = styled.div`
-  width: 136px;
-  height: 242px;
+  width: 148px;
+  height: 264px;
   background: white;
   z-index: 1;
-`
-
-export const Quadrants1 = styled.div<QuadrantsProps>`
-  width: 163.5px;
-  height: 357.5px;
-  position: absolute;
-  top: -1px;
-  left: -1.6px;
-  background-color: ${props => props.$selectedColor};
-  margin: 0.5%;
-`
-
-export const Quadrants2 = styled.div<QuadrantsProps>`
-  width: 163.5px;
-  height: 286.5px;
-  position: absolute;
-  bottom: 11.5px;
-  left: -1.6px;
-  background-color: ${props => props.$selectedColor};
-  margin: 0.5%;
-`
-
-export const Quadrants3 = styled.div<QuadrantsProps>`
-  width: 163px;
-  height: 286px;
-  position: absolute;
-  bottom: 11.5px;
-  right: -2px;
-  background-color: ${props => props.$selectedColor};
-  margin: 0.5%;
 `
