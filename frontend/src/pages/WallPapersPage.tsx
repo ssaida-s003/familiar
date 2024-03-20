b51d5970fc5297f2cda899d2f5b6166da0cd2a32
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-20 13:43:51 +0900

    🐛 fix : img 경로 에러 임시 해결

--- frontend/src/pages/WallPapersPage.tsx
+++ frontend/src/pages/WallPapersPage.tsx
@@ -3,7 +3,7 @@ import DisplayContainer from '@common/DisplayContainer'
 const WallPapersPage = () => {
   return (
     <DisplayContainer>
-      <img src="img/img_wallpapers2.png" alt="배경화면" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      <img src="/img/img_wallpapers2.png" alt="배경화면" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
     </DisplayContainer>
   )
 }
