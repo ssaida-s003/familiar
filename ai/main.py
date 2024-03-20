9720d16172bdf13139ffefe1ca16b025fef3266e
백승윤 <fangdol888@naver.com>
2024-03-20 17:49:34 +0900

    ♻️refactor: Dockerfile 빌드 태스크 줄이기 위한 리팩토링
    - 도커 파일 FROM 문 torch로 변경

--- ai/main.py
+++ ai/main.py
@@ -3,6 +3,8 @@ import uvicorn
 
 app = FastAPI()
 
+#모델 로딩
+
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World"}
@@ -10,6 +12,11 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+#가족 이미지 생성 API
+
+
+# 스케치로 그림 생성 API
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3002)
