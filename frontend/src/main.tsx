6eae77551cd4c830f0c545291e1632f522007ec7
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-20 13:28:03 +0900

    🌱 chore : css 작업시 화면 가리는 react query devtool 주석처리

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 
 import axios from 'axios'
@@ -14,7 +13,7 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/*<ReactQueryDevtools initialIsOpen={false} />*/}
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
