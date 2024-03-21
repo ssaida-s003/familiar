fc1e72238584f66a3c12a7f417e0f1ad9b5abf2f
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-21 12:00:55 +0900

    ✨ feat : header 기능 추가

--- frontend/src/common/Header.tsx
+++ frontend/src/common/Header.tsx
@@ -1,5 +1,17 @@
-const Header = () => {
-  return <div>헤더</div>
+import * as h from '@common/style/HeaderStyle'
+import React from 'react'
+
+interface HeaderProps {
+  title: string
+}
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+  return (
+    <h.Container>
+      <h.GoBack src="/icon/icon_go_back.png" />
+      <h.Title>{title}</h.Title>
+    </h.Container>
+  )
 }
 
 export default Header
