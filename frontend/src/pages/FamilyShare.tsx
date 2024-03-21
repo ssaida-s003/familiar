197a7392910ab0e67c7b114dc24e49aaaddb8240
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-21 09:48:49 +0900

    ✨ feat : 가족 공유 layout 설정

--- frontend/src/pages/FamilyShare.tsx
+++ frontend/src/pages/FamilyShare.tsx
@@ -1,9 +1,28 @@
 import DisplayContainer from '@common/DisplayContainer'
+import Header from '@common/Header'
+import ShareOrQuestion from '@components/FamilyShare/ShareOrQuestion'
+import Calender from '@components/FamilyShare/Calender'
+import Navbar from '@common/Navbar'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 const FamilyShare = () => {
   return (
     <DisplayContainer>
-      <></>
+      <Container>
+        <Header />
+        <ShareOrQuestion />
+        <Calender />
+        <Navbar />
+      </Container>
     </DisplayContainer>
   )
 }
