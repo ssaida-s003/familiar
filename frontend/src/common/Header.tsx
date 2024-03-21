705c6d8ba7bb3094f9ad78364b132791e777a5c7
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-21 12:05:32 +0900

    ✨ feat : 뒤로가기 icon 클릭 시 뒤로가기 기능 추가

--- frontend/src/common/Header.tsx
+++ frontend/src/common/Header.tsx
@@ -1,14 +1,21 @@
 import * as h from '@common/style/HeaderStyle'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 interface HeaderProps {
   title: string
 }
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
+  const navigate = useNavigate()
+  const goBack = () => {
+    console.log('뒤로가기')
+    navigate('/display')
+  }
+
   return (
     <h.Container>
-      <h.GoBack src="/icon/icon_go_back.png" />
+      <h.GoBack src="/icon/icon_go_back.png" onClick={goBack} />
       <h.Title>{title}</h.Title>
     </h.Container>
   )
