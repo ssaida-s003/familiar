aae28fb9eae2626f70145d58861257b1e33ca8e0
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-21 09:52:56 +0900

    ✨ feat : 질문 공유 혹은 하루 기록 style 적용

---
+++ frontend/src/components/FamilyShare/style/ShareOrQuestionStyle.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`
+
+export const ShareContainer = styled.div`
+  width: 90%;
+  height: 55.5px;
+  margin: 0 5%;
+  background-image: url('/icon/icon_today_share_big.png');
+  background-size: cover;
+  background-position: center;
+`
+
+export const QuestionContainer = styled.div`
+  width: 90%;
+  height: 55.5px;
+  margin: 0 5%;
+  background-image: url('/icon/icon_question_big.png');
+  background-size: cover;
+  background-position: center;
+`
+
+export const ShareOrQuestionContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-evenly;
+`
+
+export const ShareContainerSmall = styled.div`
+  width: 66px;
+  height: 90px;
+  background-image: url('/icon/icon_today_share_small.png');
+  background-size: cover;
+  background-position: center;
+`
+
+export const QuestionContainerSmall = styled.div`
+  width: 66px;
+  height: 90px;
+  background-image: url('/icon/icon_question_small.png');
+  background-size: cover;
+  background-position: center;
+`
