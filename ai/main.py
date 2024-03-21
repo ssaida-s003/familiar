7840a98861718a7d246d4fae6d2128d2ee59720c
백승윤 <fangdol888@naver.com>
2024-03-21 17:35:04 +0900

    ✨feat: API 입력부 작성
    - Fastapi API 기본 구조 작성

--- ai/main.py
+++ ai/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
 import uvicorn
 
 app = FastAPI()
@@ -9,14 +9,17 @@ app = FastAPI()
 async def read_root():
     return {"message": "Hello, World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
 #가족 이미지 생성 API
+@app.post("/diarys")
+async def make_diary_image(memberId: int, prompt: str = None):
 
+    pass
 
 # 스케치로 그림 생성 API
+@app.post("drawing/style-transfer")
+async def drawing_by_ai(title: str = Form(...), artStyle: str = Form(...), file: UploadFile = File(...)):
+
+    pass
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3002)
