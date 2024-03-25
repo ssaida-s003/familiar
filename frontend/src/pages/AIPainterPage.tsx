b9bdb2c12a6074c2e60ec770518f25874f06d5f5
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-25 15:11:16 +0900

    ✨ feat : 그림판 skeleton 코드 생성

---
+++ frontend/src/pages/AIPainterPage.tsx
@@ -0,0 +1,12 @@
+import DisplayContainer from '@common/DisplayContainer'
+import Canvas from '@components/AIPainter/Canvas'
+
+const AiPainterPage = () => {
+  return (
+    <DisplayContainer>
+      <Canvas />
+    </DisplayContainer>
+  )
+}
+
+export default AiPainterPage
