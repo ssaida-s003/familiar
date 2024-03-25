8b9f0bd5555b7f46c19a137f275ffb00f6d8d72c
김예현 <qkdo12@naver.com>
2024-03-25 20:02:34 +0900

    🌱chore: FamilyRecord Swagger 속성 수정

--- backend/src/main/java/com/ssaida/backend/haru/controller/FamilyHaruController.java
+++ backend/src/main/java/com/ssaida/backend/haru/controller/FamilyHaruController.java
@@ -21,10 +21,10 @@ import java.util.List;
 public class FamilyHaruController {
     final FamilyHaruService familyHaruService;
 
-    @GetMapping
+    @GetMapping("/monthly")
     @Operation(summary = "월별 일상공유(기록,질문,답변) 조회")
     public ResponseEntity<List> getMonthlyHaru(@Parameter(description = "가족 ID") @PathVariable("familyId") int familyId,
-                                               @RequestParam("연월 YYYY-MM") String month) {
+                                               @Parameter(description = "조회연월 yyyy-MM") @RequestParam("month") String month) {
         log.info("getMonthlyHaru 입력: {} {}", familyId, month);
 
         return new ResponseEntity<>(familyHaruService.getMonthlyHaru(familyId, month), HttpStatus.OK);
@@ -33,8 +33,7 @@ public class FamilyHaruController {
     @GetMapping("/records")
     @Operation(summary = "하루 기록 조회")
     public ResponseEntity<List> getRecords(@Parameter(description = "가족 ID") @PathVariable("familyId") int familyId,
-                                           @RequestParam("date") LocalDate date) {
-        //TODO: String으로 바꾸고 date 포맷 안맞으면 400 발생시키기
+                                           @Parameter(description = "조회일자 yyyy-MM-dd") @RequestParam("date") LocalDate date) {
         log.info("getFamilyRecords 입력: {} {}", familyId, date);
         GetRecordRequest getRecordRequest = GetRecordRequest.builder()
                 .familyId(familyId)
