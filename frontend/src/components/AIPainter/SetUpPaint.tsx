62135de65aed2eb759ca936961872401a109a2aa
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-25 17:23:51 +0900

    ✨ feat : 그림 제목 및 화풍 선택 기능 추가

---
+++ frontend/src/components/AIPainter/SetUpPaint.tsx
@@ -0,0 +1,50 @@
+import { useLocation } from 'react-router-dom'
+import DisplayContainer from '@common/DisplayContainer'
+import * as s from '@components/AIPainter/style/SetUpPaintStyle'
+
+interface CategorySetType {
+  categoryName: string
+  categoryNameByEnglish: string
+}
+
+const SetUpPaint = () => {
+  const categorySet: CategorySetType[] = [
+    {
+      categoryName: '실사화',
+      categoryNameByEnglish: 'realistic ',
+    },
+    { categoryName: '만화', categoryNameByEnglish: 'cartoon ' },
+    {
+      categoryName: '실사화',
+      categoryNameByEnglish: 'realistic ',
+    },
+    { categoryName: '만화', categoryNameByEnglish: 'cartoon ' },
+  ]
+  const location = useLocation()
+  const image = location.state?.image
+
+  return (
+    <DisplayContainer>
+      <>
+        <s.PaintContainer>{image && <s.Paint src={image} alt="Canvas Painting" />}</s.PaintContainer>
+        <s.SetUpContainer>
+          <s.TitleInfo>그림의 제목을 지어주세요!</s.TitleInfo>
+          <s.TitleInput />
+          <s.CategoryInfo> 생성할 그림의 스타일을 골라주세요!</s.CategoryInfo>
+          <s.CategoryContainer>
+            {categorySet.map(category => (
+              // eslint-disable-next-line react/jsx-key
+              <s.Category>{category.categoryName}</s.Category>
+            ))}
+          </s.CategoryContainer>
+          <s.ButtonContainer>
+            <s.ReturnButton>다시 그릴게요!</s.ReturnButton>
+            <s.MakeButton>이렇게 만들어 주세요!</s.MakeButton>
+          </s.ButtonContainer>
+        </s.SetUpContainer>
+      </>
+    </DisplayContainer>
+  )
+}
+
+export default SetUpPaint
