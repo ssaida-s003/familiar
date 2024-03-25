5c17eb8b01247bbd6052002b80f2ffd0666762aa
백승윤 <fangdol888@naver.com>
2024-03-25 16:05:34 +0900

    🐛fix: DB 실행 안되는 문제 해결
    - 콤마(,)가 들어가면 안됨 => 변수 선언부 이기 때문

--- ai/database/MyModel.py
+++ ai/database/MyModel.py
@@ -3,5 +3,5 @@ from sqlmodel import JSON, SQLModel, Field, Column, String
 class MyModel(SQLModel, table=True):
     __tablename__ = "models"
 
-    id: int = Field(default=None, primary_key=True),
+    id: int  = Field(default=None, primary_key=True)
     model_path: str | None = Column(String, default="")
