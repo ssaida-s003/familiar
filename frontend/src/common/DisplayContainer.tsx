b4cb43aa892818efbae4eeb0328c55957851fb6d
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-25 16:17:42 +0900

    🎨 style : 배경과 높이 고정

--- frontend/src/common/DisplayContainer.tsx
+++ frontend/src/common/DisplayContainer.tsx
@@ -3,8 +3,9 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   width: 444px;
+  height: 795px;
   margin: 0;
-  background-color: gray;
+  background-color: #80add9;
 `
 
 interface DisplayContainerProps {
