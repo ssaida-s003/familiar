7c3dcb0061e2a9b754982e7f311d578e473a402b
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-25 16:18:06 +0900

    🎨 style : ToolBar style 적용

--- frontend/src/components/AIPainter/ToolBar.tsx
+++ frontend/src/components/AIPainter/ToolBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 interface ToolBarProps {
   setLineWidth: (value: number) => void
@@ -9,11 +10,16 @@ interface ToolBarProps {
   setBrushColor: (color: string) => void
 }
 
+const Container = styled.div`
+  width: 100%;
+  height: 65px;
+`
+
 const ToolBar: React.FC<ToolBarProps> = ({ setLineWidth, setIsErasing, clearCanvas, undo, redo, setBrushColor }) => {
   const colors = ['#FF0000', '#FFA500', '#FFFF00', '#008000', '#0000FF', '#000080', '#800080', '#FFFFFF', '#000000']
 
   return (
-    <>
+    <Container>
       <button onClick={undo}>뒤로가기</button>
       <button onClick={redo}>앞으로가기</button>
       <button onClick={() => setIsErasing(false)}>붓</button>
@@ -23,7 +29,7 @@ const ToolBar: React.FC<ToolBarProps> = ({ setLineWidth, setIsErasing, clearCanv
       {colors.map(color => (
         <button key={color} style={{ backgroundColor: color, width: '24px', height: '24px', margin: '2px' }} onClick={() => setBrushColor(color)} />
       ))}
-    </>
+    </Container>
   )
 }
 
