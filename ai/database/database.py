1d4f06911dd0b278ac60cc45c7a8b5d37907f7bf
백승윤 <fangdol888@naver.com>
2024-03-25 18:02:22 +0900

    ♻️refactor: DB 환경변수 이름 수정

--- ai/database/database.py
+++ ai/database/database.py
@@ -14,19 +14,17 @@ class Database(object):
     Session : sessionmaker = None
 
     def __init__(self):
-        db_user = os.environ.get('DB_USER')
-        db_pass = os.environ.get('DB_PASS')
-        db_port = os.environ.get('DB_PORT')
-        db_name = os.environ.get('DB_NAME')
-        db_host = os.environ.get('DB_HOST')
+        db_user = os.environ.get('MYSQL_USERNAME')
+        db_pass = os.environ.get('MYSQL_PASSWORD')
+        db_host = os.environ.get('MYSQL_HOST')
+        db_name = os.environ.get('MYSQL_DB_NAME')
 
         self.engine = create_engine(
-            f"mysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+            f"mysql://{db_user}:{db_pass}@{db_host}/{db_name}"
         )
 
         self.session = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
         SQLModel.metadata.create_all(self.engine)
-
 
     #DB에서 알맞는 모델 불러오기 로직
     def get_model_path(self, memberId : int):
