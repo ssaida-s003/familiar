339842fbf8a64d4130f828ba538044273c556077
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-26 23:13:54 +0900

    🎨 style : axioserror style 적용

---
+++ frontend/src/common/style/AxiosErrorStyle.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const ErrorContainer = styled.div`
+  height: 745px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  font-weight: 700;
+  line-height: 100px;
+`
+export const ReturnButton = styled.button`
+  width: 48%;
+  padding: 2%;
+  border-radius: 50px;
+  border: 2px solid #d9d9d9;
+  background: linear-gradient(92deg, #d9d9d9 0.01%, rgba(255, 255, 255, 0.4) 109.3%);
+  font-weight: 700;
+  color: gray;
+`
