5350f2dbded9537a79b0deaa41852344dbd345b0
천세진 <sejinnnnnn@naver.com>
2024-03-26 10:21:23 +0900

    🐛 fix : WebMvcConfiguration CorsConfigurationSource 추가

--- backend/src/main/java/com/ssaida/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssaida/backend/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.filter.CorsFilter;
@@ -15,29 +16,21 @@ import java.util.List;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.addAllowedOriginPattern("*");
+//        configuration.addAllowedOrigin("*");
+        configuration.setAllowedOrigins(
                 List.of("http://localhost:3000", "http://localhost:5173", "https://ssaida-front.duckdns.org"));
-        config.setAllowedOriginPatterns(
+        configuration.setAllowedOriginPatterns(
                 List.of("http://localhost:3000/**", "http://localhost:5173/**", "https://ssaida-front.duckdns.org/**"));
-        config.setAllowedMethods(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-
-        config.setAllowCredentials(true);
-//		config.addAllowedOriginPattern("*");
-//		config.addAllowedHeader("*");
-//		config.addAllowedMethod("*");
-        config.setMaxAge(3600L);
-
-        source.registerCorsConfiguration("/**", config);
-
-        FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<>(new CorsFilter(source));
-        filterRegistrationBean.setOrder(0);
-
-        return filterRegistrationBean;
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
