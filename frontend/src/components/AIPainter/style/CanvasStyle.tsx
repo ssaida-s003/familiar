dc90895e3b522cab77f8fc6ed8e93d6818e78644
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-26 12:10:17 +0900

    🎨 style : 안내 문구 style 적용

--- frontend/src/components/AIPainter/style/CanvasStyle.tsx
+++ frontend/src/components/AIPainter/style/CanvasStyle.tsx
@@ -14,6 +14,14 @@ export const GalleryBtn = styled.img`
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.2));
 `
 
+export const InfoText = styled.div`
+  font-size: 12px;
+  position: absolute;
+  bottom: 150px;
+  right: 20px;
+  margin-bottom: 5px;
+`
+
 export const NextStepBtn = styled.img`
   width: 45%;
   position: absolute;
