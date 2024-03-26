6b5cb1e4b44eea1f0b19fa1e5b56e85d95d1ba25
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-26 21:15:49 +0900

    🎨 style : 기본 배경 style 수정

--- frontend/src/common/DisplayContainer.tsx
+++ frontend/src/common/DisplayContainer.tsx
@@ -3,9 +3,17 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   width: 444px;
-  height: 795px;
+  min-height: 795px;
   margin: 0;
-  background-color: #80add9;
+  background: linear-gradient(
+    163deg,
+    rgba(147, 178, 212, 0.6) 0%,
+    rgba(147, 178, 212, 0.11) 20.17%,
+    rgba(147, 178, 212, 0.6) 40.67%,
+    rgba(147, 178, 212, 0.39) 60.17%,
+    rgba(147, 178, 212, 0.6) 81.67%,
+    rgba(147, 178, 212, 0.24) 100%
+  );
 `
 
 interface DisplayContainerProps {
