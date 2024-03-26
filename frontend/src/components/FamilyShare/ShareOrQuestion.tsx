453bb4507a71c5e898a49a719036149dfe8b046c
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-26 22:15:54 +0900

    ♻️ refactor : 사용하지 않는 useEffect 삭제

--- frontend/src/components/FamilyShare/ShareOrQuestion.tsx
+++ frontend/src/components/FamilyShare/ShareOrQuestion.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import * as s from '@components/FamilyShare/style/ShareOrQuestionStyle'
 
 const ShareOrQuestion = () => {
