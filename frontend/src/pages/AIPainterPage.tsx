f34d2ace0c48556f1006c57944fdb2235c181f54
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-26 11:25:23 +0900

    ✨ feat : ai로 생성된 그림을 다시 꾸미는 경우 배경이미지로 넘기기 위해 props 추가

--- frontend/src/pages/AIPainterPage.tsx
+++ frontend/src/pages/AIPainterPage.tsx
@@ -1,10 +1,14 @@
+import { useLocation } from 'react-router-dom'
 import DisplayContainer from '@common/DisplayContainer'
 import Canvas from '@components/AIPainter/Canvas'
 
 const AiPainterPage = () => {
+  const location = useLocation()
+  const backgroundImage = location.state?.image
+
   return (
     <DisplayContainer>
-      <Canvas />
+      <Canvas backgroundImage={backgroundImage} />
     </DisplayContainer>
   )
 }
