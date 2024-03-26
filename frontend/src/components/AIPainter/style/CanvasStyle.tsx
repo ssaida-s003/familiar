1b0d8e7a423439f3d6146928cc36d9ea6b10b73d
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-26 11:25:44 +0900

    🎨 style : 갤러리 이동 버튼 style 적용

--- frontend/src/components/AIPainter/style/CanvasStyle.tsx
+++ frontend/src/components/AIPainter/style/CanvasStyle.tsx
@@ -6,6 +6,14 @@ export const Container = styled.div`
   position: relative;
 `
 
+export const GalleryBtn = styled.img`
+  width: 10%;
+  position: absolute;
+  top: 15px;
+  right: 10px;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.2));
+`
+
 export const NextStepBtn = styled.img`
   width: 45%;
   position: absolute;
