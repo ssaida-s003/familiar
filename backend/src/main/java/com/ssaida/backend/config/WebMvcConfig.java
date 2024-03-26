4b91e1a5a9d34a7c23e4405bec14868fe088ed2a
천세진 <sejinnnnnn@naver.com>
2024-03-26 16:51:21 +0900

    🐛 fix : addCorsMappings 추가

--- backend/src/main/java/com/ssaida/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssaida/backend/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.filter.CorsFilter;
 
@@ -14,6 +15,14 @@ import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOriginPatterns("http://localhost:3000", "http://localhost:5173",
+                        "http://localhost:8090", "https://ssaida-front.duckdns.org");
+    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
