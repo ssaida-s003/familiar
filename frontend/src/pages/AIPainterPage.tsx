d39c7b8ecdfefaf1232d72d060011d842d458b24
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-26 22:39:07 +0900

    ✨ feat : header 생성

--- frontend/src/pages/AIPainterPage.tsx
+++ frontend/src/pages/AIPainterPage.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from 'react-router-dom'
 import DisplayContainer from '@common/DisplayContainer'
 import Canvas from '@components/AIPainter/Canvas'
+import Header from '@common/Header'
 
 const AiPainterPage = () => {
   const location = useLocation()
@@ -8,6 +9,7 @@ const AiPainterPage = () => {
 
   return (
     <DisplayContainer>
+      <Header title={'AI 그림 그리기'} />
       <Canvas backgroundImage={backgroundImage} />
     </DisplayContainer>
   )
