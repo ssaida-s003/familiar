83a4b88d7fb0803439f2772d7529a3ba77cb18a6
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-26 20:05:36 +0900

    ✨ feat : 모든 그림을 받아오는 응답 api 생성

--- frontend/src/apis/aiPainter.ts
+++ frontend/src/apis/aiPainter.ts
@@ -1,5 +1,5 @@
 import { publicRequest } from '@hooks/requestMethods'
-import { AiPainterConvertReqType, AiPainterSaveReqType } from '@/types/aiPainter'
+import { AiPainterConvertReqType, AiPainterSaveReqType, getPaintResType } from '@/types/aiPainter'
 
 export const aiPaintConvert = async (familyId: number, data: AiPainterConvertReqType): Promise<string> => {
   return publicRequest.patch(`/families/${familyId}/drawings/convert`, data).then(res => res.data)
@@ -7,4 +7,8 @@ export const aiPaintConvert = async (familyId: number, data: AiPainterConvertReq
 
 export const aiPaintSave = async (familyId: number, data: AiPainterSaveReqType): Promise<number> => {
   return publicRequest.post(`/families/${familyId}/drawings`, data).then(res => res.data)
+}
+
+export const getAllPaintSave = async (familyId: number): Promise<getPaintResType[]> => {
+  return publicRequest.get(`/families/${familyId}/drawings`).then(res => res.data)
 }
