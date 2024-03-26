51d2da768648c498dbb258abee44491febaea596
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-26 21:15:12 +0900

    ✨ feat : 그림 card 기본 틀 생성

--- frontend/src/components/AIPainter/AIPaintCard.tsx
+++ frontend/src/components/AIPainter/AIPaintCard.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { getPaintResType } from '@/types/aiPainter'
+import * as a from '@components/AIPainter/style/AIPaintCardStyle'
 
 interface AiPaintCardProps {
   paint: getPaintResType
 }
 
 const AiPaintCard: React.FC<AiPaintCardProps> = ({ paint }) => {
-  return <div>{paint.name}</div>
+  return (
+    <a.Container>
+      <a.CardHeader>
+        <a.Title>{paint.name}</a.Title>
+        <a.PinIcon src="/icon/icon_pin.png" />
+        <a.SnowManIcon src="/icon/icon_snowman.png" />
+      </a.CardHeader>
+      <a.CreatedDate>{paint.createdAt}</a.CreatedDate>
+      <a.PaintContainer>
+        <a.Img src="/img/img_sample_wallpapers.png"></a.Img>
+        <a.ArrowIcon src="/icon/icon_arrow.png"></a.ArrowIcon>
+        <a.Img src="/img/img_sample_wallpapers.png"></a.Img>
+      </a.PaintContainer>
+    </a.Container>
+  )
 }
 
 export default AiPaintCard
