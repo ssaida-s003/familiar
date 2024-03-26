1e9da761db936d9ae91ce207a28e65ebdb971ff6
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-27 02:18:06 +0900

    🎨 style : ShareOrQuestion px 단위를 rem과 vh로 수정

--- frontend/src/components/FamilyShare/style/ShareOrQuestionStyle.tsx
+++ frontend/src/components/FamilyShare/style/ShareOrQuestionStyle.tsx
@@ -6,11 +6,14 @@ interface ShareOrQuestionProps {
 
 export const Container = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 3%;
 `
 
 export const SelectedContainer = styled.div<ShareOrQuestionProps>`
   width: 90%;
-  height: 55.3px;
+  height: 21.3vh;
   margin: 0 5%;
   background-image: url(${props => (props.$shareOrQuestion === 1 ? '/icon/icon_today_share_big.png' : '/icon/icon_question_big.png')});
   background-size: cover;
@@ -18,22 +21,21 @@ export const SelectedContainer = styled.div<ShareOrQuestionProps>`
 `
 
 export const ShareOrQuestionContainer = styled.div`
-  width: 100%;
+  width: 90%;
+  height: 32vh;
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
 `
 
 export const ShareContainerSmall = styled.div`
-  width: 66.4px;
-  height: 90.6px;
+  width: 46%;
   background-image: url('/icon/icon_today_share_small.png');
   background-size: cover;
-  background-position: bottom;
+  background-position: center;
 `
 
 export const QuestionContainerSmall = styled.div`
-  width: 66.4px;
-  height: 90.9px;
+  width: 46%;
   background-image: url('/icon/icon_question_small.png');
   background-size: cover;
   background-position: center;
