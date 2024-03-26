8f38c0ca402d58f17caf4873bae827caaf0e875f
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-26 20:20:05 +0900

    ✨ feat : 그림 리스트를 볼 수 잇는 album page 생성

--- frontend/src/pages/AIPaintAlbumPage.tsx
+++ frontend/src/pages/AIPaintAlbumPage.tsx
@@ -1,11 +1,13 @@
 import DisplayContainer from '@common/DisplayContainer'
 import Header from '@common/Header'
+import AIPaintList from '@components/AIPainter/AIPaintList'
 
 const AIPaintAlbumPage = () => {
   return (
     <DisplayContainer>
       <>
         <Header title={'AI 그림판'} />
+        <AIPaintList />
       </>
     </DisplayContainer>
   )
