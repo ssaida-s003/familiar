fdb1a4b7fa3a4d6f47da65165f32664c5b435dca
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-28 02:52:30 +0900

    ✨ feat : 생성된 이미지의 경로와 녹음 내용을 기반을 canvas의 props로 전달

--- frontend/src/components/FamilyShare/AfterRecord.tsx
+++ frontend/src/components/FamilyShare/AfterRecord.tsx
@@ -1,10 +1,12 @@
 import * as a from '@components/FamilyShare/style/AfterRecord'
+import ImageCanvas from '@components/FamilyShare/ImageCanvas'
 
 const AfterRecord = () => {
   // const { image, content } = useConvertTodayStore()
   return (
     <>
       <a.ImgContainer>
+        <ImageCanvas imageUrl={'/img/img_wallpapers1.png'} content={'오늘은 친구랑 서울 숲에 다녀왔어'} />
         <a.InfoText>텍스트의 위치를 바꿀 수 있어요!</a.InfoText>
       </a.ImgContainer>
       <a.ButtonContainer>
