26b850be3630b2666a2a6b47d44fac8ef73734e2
백승윤 <fangdol888@naver.com>
2024-03-27 17:45:08 +0900

    ✨feat: 일기 사진 생성 API 작성
    - 모델 로딩 로직 추가
    - 베이스 모델에 따라서 선택할 수 있도록 수정

--- ai/main.py
+++ ai/main.py
@@ -2,12 +2,23 @@ import uvicorn
 from fastapi import FastAPI, UploadFile, File, Form
 from starlette.middleware.cors import CORSMiddleware
 
+import os
+
+from dto.DiaryRequestDto import DiaryRequestDto
+from dto.DrawingRequestDto import DrawingRequestDto
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
 from database.Configs import Config
-from database.MyModel import MyModel
+from database.Models import Models
 from database import database
 from model import InferenceParameter
-from .model.RV2_0 import RV2_0
+from model.model_selector import model_selector
+from model.RV2_0 import RV2_0
 from PIL import Image
+import argparse, sys
+from fastapi.responses import StreamingResponse
+import io
 
 app = FastAPI()
 
@@ -43,25 +54,33 @@ async def read_root():
 
 #가족 이미지 생성 API
 @app.post("/diarys")
-async def make_diary_image(memberId: int, prompt: str = None):
-    model_path : MyModel = db.get_model_path(memberId) # 맴버에 맞는 모델 불러오기
-    config: Config = db.get_configs(model_path) # 모델에 맞는 설정 파일 들고 오기
+async def make_diary_image(requestDto : DiaryRequestDto):
+    model_info : Models = db.get_model_info(requestDto.memberId) # 맴버에 맞는 모델 불러오기
+    config: Config = db.get_configs(model_info.model_path) # 모델에 맞는 설정 파일 들고 오기
 
     #추론 수행 모델 불러오기 및 추론 수행
-    if models['diary'] != config.model_path : # 만약 현재 수행 중인 모델이 현재 모델이 아니라면
-        models['diary'] = RV2_0(config) #모델 정보 불러오기
+    if models['diary'] == None or models['diary'].config.model_path != config.model_path : # 만약 현재 수행 중인 모델이 없거나 현재 모델이 아니라면
+        myModel = model_selector[config.base_model]
+        models['diary'] = myModel(model_info, config) #모델 정보 불러오기
 
-    input : InferenceParameter = InferenceParameter({'prompt': prompt})
+    input : InferenceParameter = InferenceParameter({'prompt': requestDto.prompt})
     result : Image = models['diary'].inference(input) # 모델 추론 결과
 
-    return result
+    # PIL 이미지를 바이트로 변환
+    img_byte_array = io.BytesIO()
+    result.save(img_byte_array, format="JPEG")
+    img_byte_array.seek(0)
 
+    return StreamingResponse(img_byte_array, media_type="image/jpeg")
 
 # 스케치로 그림 생성 API
 @app.post("drawing/style-transfer")
-async def drawing_by_ai(title: str = Form(...), artStyle: str = Form(...), file: UploadFile = File(...)):
+async def drawing_by_ai(requestDto : DrawingRequestDto):
 
     pass
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=3002)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--reload', action='store_true', help=' : debug mode', default=False)
+    args = parser.parse_args()
+    uvicorn.run("main:app", host="0.0.0.0", port=3002 , reload=args.reload)
---
+++ ai/model/model_selector.py
@@ -0,0 +1,7 @@
+from model.RV2_0 import RV2_0
+from model.RV6_0 import RV6_0
+
+model_selector = {
+    "RV2_0": RV2_0,
+    "RV6_0": RV6_0
+}
