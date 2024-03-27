5814e4c341a2a3477cbe9f34d0ea70b5fc28355a
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-27 21:00:30 +0900

    ✨ feat : 녹음이 진행될 때 보이는 recording component 생성

--- frontend/src/components/FamilyShare/Recording.tsx
+++ frontend/src/components/FamilyShare/Recording.tsx
@@ -1,5 +1,60 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+import * as r from '@components/FamilyShare/style/RecordingStyle'
+import useSpeechToText from '@hooks/useSpeechToText'
+import AudioVisualizer from '@components/FamilyShare/AudioVisualizer'
+
 const Recording = () => {
-  return <div></div>
+  const [result, startListening, stopListening] = useSpeechToText()
+  const [inputText, setInputText] = useState('')
+  const [isEditing, setIsEditing] = useState(false)
+  const [isCompleteRecord, setIsCompleteRecord] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      !isCompleteRecord && startListening()
+    }, 500)
+  }, [startListening, isCompleteRecord])
+
+  const handleStopRecord = () => {
+    stopListening()
+    setIsCompleteRecord(true)
+  }
+
+  useEffect(() => {
+    if (!isEditing) {
+      setInputText(result.text)
+    }
+  }, [result.text, isEditing])
+
+  const handleReRecord = () => {
+    setInputText('')
+    setIsEditing(false)
+    setIsCompleteRecord(false)
+    startListening()
+  }
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setIsEditing(true)
+    setInputText(e.target.value)
+    setIsEditing(false)
+  }
+
+  const handleSubmit = () => {}
+
+  return (
+    <>
+      <r.TopContainer>
+        <AudioVisualizer isCompleted={isCompleteRecord} />
+      </r.TopContainer>
+
+      <r.BottomContainer>
+        <r.TextAreaInput value={inputText} onChange={handleInputChange} />
+        <r.ButtonContainer>
+          {isCompleteRecord ? <r.ReRecordBtn onClick={handleReRecord}>다시 녹음할래요!</r.ReRecordBtn> : <r.CompleteRecordBtn onClick={handleStopRecord}>녹음 그만 할게요!</r.CompleteRecordBtn>}
+          <r.GoNextStepBtn onClick={handleSubmit}>이대로 할게요!</r.GoNextStepBtn>
+        </r.ButtonContainer>
+      </r.BottomContainer>
+    </>
+  )
 }
 
 export default Recording
