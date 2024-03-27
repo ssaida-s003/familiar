705a5cbb1223735c080b7cf6a16031459c151566
김예현 <qkdo12@naver.com>
2024-03-27 22:15:40 +0900

    🐛fix: 기록, 답변 저장 시에 버킷에 이미지 저장

--- backend/src/main/java/com/ssaida/backend/haru/service/ConvertHaruService.java
+++ backend/src/main/java/com/ssaida/backend/haru/service/ConvertHaruService.java
@@ -3,5 +3,5 @@ package com.ssaida.backend.haru.service;
 import com.ssaida.backend.haru.dto.ConvertRecordRequest;
 
 public interface ConvertHaruService {
-    byte[] convertHaru(ConvertRecordRequest convertRecordRequest);
+    String convertHaru(ConvertRecordRequest convertRecordRequest);
 }
--- backend/src/main/java/com/ssaida/backend/haru/service/RecordService.java
+++ backend/src/main/java/com/ssaida/backend/haru/service/RecordService.java
@@ -3,6 +3,8 @@ package com.ssaida.backend.haru.service;
 import com.ssaida.backend.haru.dto.ConvertRecordRequest;
 import com.ssaida.backend.haru.dto.CreateRecordRequest;
 
+import java.io.IOException;
+
 public interface RecordService {
     void createRecord(CreateRecordRequest createRecordRequest);
 
