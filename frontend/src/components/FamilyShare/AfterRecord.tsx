a6970a3d002537bf2a841480a555fc670c7f5dcb
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-28 02:50:28 +0900

    ✨ feat : 생성되 배경을 확인하는 컴포넌트 생성

--- frontend/src/components/FamilyShare/AfterRecord.tsx
+++ frontend/src/components/FamilyShare/AfterRecord.tsx
@@ -1,5 +1,18 @@
+import * as a from '@components/FamilyShare/style/AfterRecord'
+
 const AfterRecord = () => {
-  return <div></div>
+  // const { image, content } = useConvertTodayStore()
+  return (
+    <>
+      <a.ImgContainer>
+        <a.InfoText>텍스트의 위치를 바꿀 수 있어요!</a.InfoText>
+      </a.ImgContainer>
+      <a.ButtonContainer>
+        <a.ReConvertBtn>다시 생성할래요!</a.ReConvertBtn>
+        <a.GoNextStepBtn>이대로 답변할게요!</a.GoNextStepBtn>
+      </a.ButtonContainer>
+    </>
+  )
 }
 
 export default AfterRecord
