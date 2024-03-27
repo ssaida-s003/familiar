3c775537baf5c13a93b65bb8cc453d36a8e156fb
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-28 01:23:26 +0900

    ✨ feat : 그림 저장 store 생성

--- frontend/src/stores/familyShare.ts
+++ frontend/src/stores/familyShare.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { ShareQnAState, ShareStepState } from '@/types/familyShare'
+import { ConvertTodayState, ShareQnAState, ShareStepState } from '@/types/familyShare'
 
 export const useShareStepStore = create<ShareStepState>(set => ({
   shareStep: 0,
@@ -9,4 +9,13 @@ export const useShareStepStore = create<ShareStepState>(set => ({
 export const useQnAStepStore = create<ShareQnAState>(set => ({
   qnaStep: 0,
   setQnAStep: (step: number) => set({ qnaStep: step }),
+}))
+
+export const useConvertTodayStore = create<ConvertTodayState>(set => ({
+  memberId: 0,
+  content: '',
+  image: '',
+  setMemberId: (memberId: number) => set({ memberId: memberId }),
+  setContent: (content: string) => set({ content: content }),
+  setImage: (image: string) => set({ image: image }),
 }))
