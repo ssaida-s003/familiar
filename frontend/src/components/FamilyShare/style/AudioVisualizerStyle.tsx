1c93204635e6c52029b15ac713f25467a72d8c09
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-27 17:51:14 +0900

    🎨 style : AudioVisualizer style 미세 수정

--- frontend/src/components/FamilyShare/style/AudioVisualizerStyle.tsx
+++ frontend/src/components/FamilyShare/style/AudioVisualizerStyle.tsx
@@ -5,7 +5,7 @@ export const VisualizerContainer = styled.div`
   align-items: center;
   height: 100%;
   width: 100%;
-  gap: 2px;
+  gap: 4px;
 `
 
 export const Bar = styled.div.attrs<{ height: number }>(({ height }) => ({
@@ -13,8 +13,10 @@ export const Bar = styled.div.attrs<{ height: number }>(({ height }) => ({
     height: `${height}%`,
   },
 }))<{ height: number }>`
-  background-color: red;
-  width: 5px;
+  background-color: black;
+  min-height: 10px;
+  width: 3px;
   transition: height 0.05s ease-in-out;
   border-radius: 1rem;
+  filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.25));
 `
