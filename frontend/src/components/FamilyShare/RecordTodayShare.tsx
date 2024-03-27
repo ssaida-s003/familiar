2a88aee7d9b187128dff71e64a161ef2f941af7b
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-27 10:04:55 +0900

    🎨 style : RecordTodayShare Container style 적용

--- frontend/src/components/FamilyShare/RecordTodayShare.tsx
+++ frontend/src/components/FamilyShare/RecordTodayShare.tsx
@@ -1,17 +1,23 @@
-import * as r from '@components/FamilyShare/style/RecordTodayShareStyle'
 import BeforeRecord from '@components/FamilyShare/BeforeRecord'
 import Recording from '@components/FamilyShare/Recording'
 import AfterRecord from '@components/FamilyShare/AfterRecord'
 import { useShareStepStore } from '@stores/familyShare'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  height: 32vh;
+  margin: 3% 0;
+`
 
 const RecordTodayShare = () => {
   const { step } = useShareStepStore()
   return (
-    <r.Container>
+    <Container>
       {step == 0 && <BeforeRecord />}
       {step == 1 && <Recording />}
       {step == 2 && <AfterRecord />}
-    </r.Container>
+    </Container>
   )
 }
 
