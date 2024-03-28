fa47b2fea5559c790cf9d045d1ccf29fb30ed9e8
백승윤 <fangdol888@naver.com>
2024-03-28 13:11:25 +0900

    ✨feat: 로라 판단 및 생성 키워드 컬럼 추가

--- ai/database/Models.py
+++ ai/database/Models.py
@@ -5,3 +5,5 @@ class Models(SQLModel, table=True):
     model_path: str = Field(max_length=1000, description='모델 로컬 위치', primary_key=True)
     member_id: int = Field(description='맴버 아이디')
     model_repo: str = Field(max_length=1000, description='모델 레포지토리 주소')
+    gen_keyword : str = Field(description='드림부스 생성 키워드')
+    is_lora: bool = Field(default=False, description='LoRA 모델 인지 확인')
