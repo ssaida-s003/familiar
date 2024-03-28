2b08911f45799dd4c1bf0171cbda8ed446ea2611
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-28 14:11:51 +0900

    ✨ feat : 재생성, 저장 api 연결

--- frontend/src/components/FamilyShare/AfterRecord.tsx
+++ frontend/src/components/FamilyShare/AfterRecord.tsx
@@ -1,8 +1,40 @@
-import * as a from '@components/FamilyShare/style/AfterRecord'
+import * as a from '@components/FamilyShare/style/AfterRecordStyle.tsx'
 import ImageCanvas from '@components/FamilyShare/ImageCanvas'
+import { useMutation } from 'react-query'
+import { familyTodayConvert, postToday } from '@apis/familyShare'
+import { useConvertTodayStore, useShareStepStore } from '@stores/familyShare'
+import ConvertRecord from '@components/FamilyShare/ConvertRecord.tsx'
+import { useNavigate } from 'react-router-dom'
 
 const AfterRecord = () => {
-  // const { image, content } = useConvertTodayStore()
+  const { image, content, setImage } = useConvertTodayStore()
+  const { setShareStep } = useShareStepStore()
+  const navigate = useNavigate()
+
+  const reConvertMutation = useMutation(familyTodayConvert, {
+    onSuccess: data => {
+      setImage(data)
+    },
+  })
+
+  const postTodayMutation = useMutation(postToday, {
+    onSuccess: () => {
+      setShareStep(0)
+      navigate('/display/share-family')
+    },
+  })
+
+  const handleReConvert = () => {
+    reConvertMutation.mutate({ memberId: 1, content: content })
+    if (reConvertMutation.isLoading) {
+      return <ConvertRecord />
+    }
+  }
+
+  const handlePost = () => {
+    postTodayMutation.mutate({ memberId: 3, content: '오늘은 친구랑 서울 숲에 다녀왔어', url: 'asdfasdfsadf' })
+  }
+
   return (
     <>
       <a.ImgContainer>
@@ -10,8 +42,8 @@ const AfterRecord = () => {
         <a.InfoText>텍스트의 위치를 바꿀 수 있어요!</a.InfoText>
       </a.ImgContainer>
       <a.ButtonContainer>
-        <a.ReConvertBtn>다시 생성할래요!</a.ReConvertBtn>
-        <a.GoNextStepBtn>이대로 답변할게요!</a.GoNextStepBtn>
+        <a.ReConvertBtn onClick={handleReConvert}>다시 생성할래요!</a.ReConvertBtn>
+        <a.GoNextStepBtn onClick={handlePost}>이대로 답변할게요!</a.GoNextStepBtn>
       </a.ButtonContainer>
     </>
   )
