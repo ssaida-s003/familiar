90ff825cde8fe496c69ed4a447c363c1cd9f4307
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-28 23:37:09 +0900

    🐛 fix : 요청 객체를 boday가 아닌 params로 전달

--- frontend/src/apis/calender.ts
+++ frontend/src/apis/calender.ts
@@ -2,9 +2,9 @@ import { publicRequest } from '@hooks/requestMethods'
 import { DateReqType, FamilyShareRecordResType, QnARecordResType } from '@/types/calender'
 
 export const fetchFamilyShareRecord = async (familyId: number, data: DateReqType): Promise<FamilyShareRecordResType> => {
-  return publicRequest.get(`/families/${familyId}/haru/records`, data).then(res => res.data)
+  return publicRequest.get(`/families/${familyId}/haru/records`, { params: data }).then(res => res.data)
 }
 
 export const fetchQnARecord = async (familyId: number, data: DateReqType): Promise<QnARecordResType> => {
-  return publicRequest.get(`/families/${familyId}/haru/questions`, data).then(res => res.data)
+  return publicRequest.get(`/families/${familyId}/haru/questions`, { params: data }).then(res => res.data)
 }
