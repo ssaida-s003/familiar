6bc384a0e097ffe003f1462172d75946f63cad26
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-29 01:03:24 +0900

    🐛 fix : 응답 객체 배열로 수정

--- frontend/src/apis/calender.ts
+++ frontend/src/apis/calender.ts
@@ -1,7 +1,7 @@
 import { publicRequest } from '@hooks/requestMethods'
 import { DateReqType, FamilyShareRecordResType, QnARecordResType } from '@/types/calender'
 
-export const fetchFamilyShareRecord = async (familyId: number, data: DateReqType): Promise<FamilyShareRecordResType> => {
+export const fetchFamilyShareRecord = async (familyId: number, data: DateReqType): Promise<FamilyShareRecordResType[]> => {
   return publicRequest.get(`/families/${familyId}/haru/records`, { params: data }).then(res => res.data)
 }
 
