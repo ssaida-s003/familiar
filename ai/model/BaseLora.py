9d4100210b23633963e7e5079dce6eedf8212f83
백승윤 <fangdol888@naver.com>
2024-03-29 15:35:08 +0900

    ♻️refactor: 코드 분리 및 LoRA 붙였다 떼는 로직 추가

---
+++ ai/model/BaseLora.py
@@ -0,0 +1,16 @@
+from diffusers import StableDiffusionPipeline
+
+from database.Configs import Config
+from database.Models import Models
+from model.base import BaseModel
+
+
+class BaseLoraModel(BaseModel):
+    def __init__(self, model_info : Models,  config: Config):
+        super().__init__(model_info, config)
+
+    def attach_lora(self, adpater_name:str = "person"):
+        local_path = self.model_info.model_path[:-(len(self.model_info.lora_filename) + 1)]
+        self.pipe.load_lora_weights(local_path, weight_name=self.model_info.lora_filename + ".safetensors", adapter_name=adpater_name)
+    def detach_lora(self):
+        self.pipe.unload_lora_weights()
