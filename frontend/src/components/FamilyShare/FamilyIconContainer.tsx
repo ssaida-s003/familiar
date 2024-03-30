64722b289b8cd4cf7f77f7130e6eac45e841f33a
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-30 11:35:49 +0900

    🎨 style : IconContainer style 적용

--- frontend/src/components/FamilyShare/FamilyIconContainer.tsx
+++ frontend/src/components/FamilyShare/FamilyIconContainer.tsx
@@ -1,3 +1,17 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+`
+
+const FamilyIcon = styled.img`
+  width: 1.8rem;
+  padding: 2px;
+  margin: 2px;
+  border-radius: 1rem;
+  background: skyblue;
+`
+
 const FamilyIconContainer = () => {
   const family = ['mother', 'father', 'son', 'daughter']
 
