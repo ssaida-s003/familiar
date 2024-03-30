1f3c0c9eb001012984482be03eb2ea8ec5666c81
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-30 15:48:59 +0900

    🐛 fix : 가족공유 메인 페이지 컴포넌트 구성 변경

--- frontend/src/pages/FamilyShare.tsx
+++ frontend/src/pages/FamilyShare.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import styled from 'styled-components'
 import Header from '@common/Header'
 import DisplayContainer from '@common/DisplayContainer'
 import ShareOrQuestion from '@components/FamilyShare/ShareOrQuestion'
@@ -9,12 +8,6 @@ import RecordTodayShare from '@components/FamilyShare/RecordTodayShare'
 import RecordQnA from '@components/FamilyShare/RecordQnA'
 import { useShareStepStore } from '@stores/familyShare.ts'
 
-const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  justify-content: center;
-`
-
 const FamilyShare = () => {
   const [isTodayShareRecord, setIsTodayShareRecord] = useState(false)
   const [isQnARecord, setIsQnARecord] = useState(false)
@@ -22,15 +15,11 @@ const FamilyShare = () => {
 
   return (
     <DisplayContainer>
-      <>
-        <Container>
-          <Header title={'가족 공유'} />
-          {shareStep === 0 && <ShareOrQuestion setIsTodayShareRecord={setIsTodayShareRecord} setIsQnARecord={setIsQnARecord} />}
-          {shareStep !== 0 && isTodayShareRecord ? <RecordTodayShare /> : isQnARecord && <RecordQnA />}
-          <AIImgAnswer />
-          <Calender />
-        </Container>
-      </>
+      <Header title={'가족 공유'} />
+      {shareStep === 0 && <ShareOrQuestion setIsTodayShareRecord={setIsTodayShareRecord} setIsQnARecord={setIsQnARecord} />}
+      {shareStep !== 0 && isTodayShareRecord ? <RecordTodayShare /> : isQnARecord && <RecordQnA />}
+      <AIImgAnswer />
+      <Calender />
     </DisplayContainer>
   )
 }
