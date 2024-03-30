c35176e497e8d10b5a7fc4a045c1f1b2812ac5d9
KIMHYEONJI <hyeonji0718@gmail.com>
2024-03-30 15:49:50 +0900

    🎨 style : DisplayContainer style 수정

--- frontend/src/common/DisplayContainer.tsx
+++ frontend/src/common/DisplayContainer.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
 
-const Container = styled.div`
+interface ContainerProps {
+  $paddingTop: string
+}
+
+const Container = styled.div<ContainerProps>`
   box-sizing: border-box;
   width: 444px;
-  min-height: 795px;
-  padding-top: 50px;
+  height: 840px;
+  padding-top: ${props => props.$paddingTop};
+
   background: linear-gradient(
       163deg,
       rgba(65, 142, 176, 0.3) 0%,
@@ -17,7 +22,24 @@ const Container = styled.div`
       rgba(65, 142, 176, 0.4) 100%
     ),
     white;
-  overflow: scroll;
+  overflow: auto;
+`
+
+const Footer = styled.footer`
+  width: 444px;
+  height: 40px;
+  background: #444;
+  align-items: center;
+  justify-content: space-around;
+  position: fixed;
+  top: 840px;
+  display: flex;
+`
+
+const Icon = styled.img`
+  width: 20px;
+  height: 20px;
+  padding: 5px;
 `
 
 interface DisplayContainerProps {
@@ -25,9 +47,19 @@ interface DisplayContainerProps {
 }
 
 const DisplayContainer: React.FC<DisplayContainerProps> = ({ children }) => {
-  return <Container>{children} </Container>
   const navigate = useNavigate()
   const paddingTop = location.pathname === '/display/wallpapers' ? '0px' : '50px'
+
+  return (
+    <Container $paddingTop={paddingTop}>
+      {children}
+      <Footer>
+        <Icon src={'/icon/icon_record.png'} onClick={() => navigate('/display/share-family')} />
+        <Icon src={'/icon/icon_wallPapers.png'} onClick={() => navigate('/display/wallpapers')} />
+        <Icon src={'/icon/icon_palette.png'} onClick={() => navigate('/display/AI-painter')} />
+      </Footer>
+    </Container>
+  )
 }
 
 export default DisplayContainer
