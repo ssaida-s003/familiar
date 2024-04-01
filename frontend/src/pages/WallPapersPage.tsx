d683d0614f85d31fd431dcb6320f04c289f3adfc
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-02 02:31:57 +0900

    ✨ feat : 하루 공유 바탕화면에 보이도록 설정

--- frontend/src/pages/WallPapersPage.tsx
+++ frontend/src/pages/WallPapersPage.tsx
@@ -3,6 +3,9 @@ import DisplayContainer from '@common/DisplayContainer'
 import styled from 'styled-components'
 import { useBackgroundStore } from '@stores/background'
 import { useThemeStore } from '@stores/theme'
+import { useQueries } from 'react-query'
+import { fetchFamilyShareRecord, fetchQnARecord } from '@apis/calender'
+import { useTodayDateStore } from '@stores/calendar'
 
 interface themeProps {
   $mainColor: string
@@ -18,19 +21,74 @@ const NextBtn = styled.img<themeProps>`
   border-radius: 10rem;
 `
 
+const ChangeBtn = styled.img`
+  position: absolute;
+  bottom: 50px;
+  left: 15px;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+`
+
 const WallPapersPage = () => {
   const { paintBackground } = useBackgroundStore()
   const { mainColor } = useThemeStore()
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(1)
+  const [backgroundType, setBackgroundType] = useState(0)
+  const [currentIndex2, setCurrentIndex2] = useState(0)
+  const { date } = useTodayDateStore()
 
   // 배경화면을 넘기는 함수
   const handleNextClick = () => {
-    setCurrentIndex(prevIndex => (prevIndex + 1) % paintBackground.length)
+    if (backgroundType === 1) {
+      setCurrentIndex(prevIndex => (prevIndex + 1) % paintBackground.length)
+    } else if (backgroundType === 2 && shareRecord) {
+      setCurrentIndex2(prevIndex => (prevIndex + 1) % shareRecord.length)
+    }
+  }
+
+  const queryResults = useQueries([
+    {
+      queryKey: ['familyShareRecord', 1, date],
+      queryFn: () => fetchFamilyShareRecord(1, { date }),
+      enabled: !!date,
+    },
+    {
+      queryKey: ['qnaRecord', 1, date],
+      queryFn: () => fetchQnARecord(1, { date }),
+      enabled: !!date,
+    },
+  ])
+
+  const shareRecord = queryResults[0].data
+
+  const handleChangeClick = () => {
+    if (backgroundType === 1) {
+      setBackgroundType(2)
+    } else {
+      setBackgroundType(1)
+    }
   }
 
   return (
     <DisplayContainer title={'배경화면'}>
-      {paintBackground.length > 0 && <img key={currentIndex} src={paintBackground[currentIndex]} alt="배경화면" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />}
+      <ChangeBtn onClick={handleChangeClick}></ChangeBtn>
+      {backgroundType === 1 && paintBackground.length > 0 && (
+        <img key={currentIndex} src={paintBackground[currentIndex]} alt="배경화면" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      )}
+      {backgroundType === 2 && shareRecord && (
+        <img
+          key={currentIndex2}
+          src={shareRecord[currentIndex2].url}
+          alt="배경화면"
+          style={{
+            width: '100%',
+            height: 'auto',
+            margin: '35% 0 0 0',
+            objectFit: 'cover',
+          }}
+        />
+      )}
       <NextBtn $mainColor={mainColor} src="/icon/icon_next.png" onClick={handleNextClick}></NextBtn>
     </DisplayContainer>
   )
