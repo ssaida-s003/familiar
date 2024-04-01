66f6ff724fa641eb06f88a2ba69fb0c55df25400
KIMHYEONJI <hyeonji0718@gmail.com>
2024-04-01 22:40:11 +0900

    🐛 fix : 색상 코드 앞 # 추가

--- frontend/src/components/AIPainter/style/SetUpPaintStyle.tsx
+++ frontend/src/components/AIPainter/style/SetUpPaintStyle.tsx
@@ -78,7 +78,7 @@ export const Category = styled.div<CategoryProps>`
   border-radius: 100%;
   background: #fff;
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-  border: 3px solid ${({ $isSelected, $mainColor }) => ($isSelected ? $mainColor : 'transparent')};
+  border: 3px solid ${({ $isSelected, $mainColor }) => ($isSelected ? `#${$mainColor}` : 'transparent')};
 `
 
 export const ButtonContainer = styled.div`
